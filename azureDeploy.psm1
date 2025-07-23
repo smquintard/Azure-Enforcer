be7c07af7bf3ba8229777ca2cddc27f07586f2b0
Shane <55709643+smquintard@users.noreply.github.com>
2025-07-22 23:12:11 -0400

    Update azureDeploy.psm1

--- azureDeploy.psm1
+++ azureDeploy.psm1
@@ -89,21 +89,175 @@ function Set-FunctionAppSettings {
     }
 }
 
-function Clone-And-Build-Project {
-    Write-Host "Cloning and building project..." --ForegroundColor Cyan
-    if (-not (Test-Path "azure-enforcer-template")) {
-        git clone https://github.com/PerimeterX/azure-enforcer-template.git
-    }
-    Push-Location azure-enforcer-template
-    npm install
-    npm run build
-    Pop-Location
-}
-
 function Publish-FunctionApp {
     param([hashtable]$Config)
     Push-Location azure-enforcer-template
     Write-Host "Publishing Function App..." -ForegroundColor Cyan
     func azure functionapp publish $Config.FUNCTION_APP_NAME --typescript
     Pop-Location
+}
+
+function Configure-FrontDoorRuleset {
+    param([hashtable]$Config)
+
+    Write-Host "Creating Front Door Ruleset..." -ForegroundColor Cyan
+    az afd rule-set create
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE | Out-Null
+        
+    #Rule 1 - Header Injection
+    Write-Host "Creating Rule 1 - Header Injection..." -ForegroundColor Cyan
+    az afd rule create
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --order 1
+        --match-variable UrlPath
+        --operator BeginsWith
+        --match-values "/$($Config.PX_APPID.Substring(0,6))/"
+        --rule-name HSFirstPartyHeaders
+        --action-name ModifyRequestHeader
+        --header-action Append
+        --header-name "x-px-first-party"
+        --header-value "1"
+        --match-processing-behavior Continue | Out-Null
+
+    az afd rule action function
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --rule-name HSFirstPartyHeaders
+        --action-name ModifyRequestHeader
+        --header-action Append
+        --header-name "x-px-enforcer-true-ip"
+        --header-value "{client_ip}" | Out-Null
+
+    #Rule 2 - JS Rewrite
+    Write-Host "Creating Rule 2 - JS Rewrite..." -ForegroundColor Cyan
+    az afd rule create
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --order 2
+        --match-variable UrlPath
+        --operator Equal
+        --match-values "/$($Config.PX_APPID.Substring(0,6))/init.js"
+        --rule-name HSFirstPartyClient
+        --action-name UrlRewrite
+        --source-pattern "/$($Config.PX_APPID.Substring(0,6))/init.js"
+        --destination "/$($Config.PX_APP_ID)/main.min.js"
+        --preserve-unmatched-path No
+        --match-processing-behavior Stop | Out-Null
+
+    az afd rule action add
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --rule-name HSFirstPartyClient
+        --action-name RouteConfigurationOverride
+        --origin-group HSClient
+        --forwarding-protocol MatchRequest
+        --enable-caching false | Out-Null
+
+    #Rule 3 Captcha Rewrite
+    Write-Host "Creating Rule 3 - Captcha Rewrite..." -ForegroundColor Cyan
+    az afd rule create
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --order 3
+        --match-variable UrlPath
+        --operator BeginsWith
+        --match-values "/$($Config.PX_APPID.Substring(0,6))/captcha"
+        --rule-name HSFirstPartyCaptcha
+        --action-name UrlRewrite
+        --source-pattern "/$($Config.PX_APPID.Substring(0,6))/captcha"
+        --destination "/$($Config.PX_APPID)"
+        --preserve-unmatched-path Yes
+        --match-processing-behavior Stop | Out-Null
+
+    az afd rule action add
+        --resource-group $Config.RESOURCE_GROUP
+        --rule-set-name $Config.RULESET_NAME
+        --profile-name $Config.FRONT_DOOR_PROFILE
+        --rule-name HSFirstPartyCaptcha
+        --action-name RouteConfigurationOverride
+        --origin-group HSCaptcha
+        --forwarding-protocol MatchRequest
+        --enable-caching false | Out-Null
+
+    # Rule 4 - XHR Rewrite
+    Write-Host "Creating Rule 4 (XHR Rewrite)..." -ForegroundColor Cyan
+    az afd rule create `
+        --resource-group $Config.RESOURCE_GROUP `
+        --rule-set-name $Config.RULESET_NAME `
+        --profile-name $Config.FRONT_DOOR_PROFILE `
+        --order 4 `
+        --match-variable UrlPath `
+        --operator BeginsWith `
+        --match-values "/$($Config.PX_APPID.Substring(0,6))/xhr" `
+        --rule-name HSFirstPartyXHR `
+        --action-name UrlRewrite `
+        --source-pattern "/$($Config.PX_APPID.Substring(0,6))/xhr" `
+        --destination "/" `
+        --preserve-unmatched-path Yes `
+        --match-processing-behavior Stop | Out-Null
+
+    az afd rule action add `
+        --resource-group $Config.RESOURCE_GROUP `
+        --rule-set-name $Config.RULESET_NAME `
+        --profile-name $Config.FRONT_DOOR_PROFILE `
+        --rule-name HSFirstPartyXHR `
+        --action-name RouteConfigurationOverride `
+        --origin-group HSCollector `
+        --forwarding-protocol MatchRequest `
+        --enable-caching false | Out-Null
+
+    # Rule 5 - Header overwrite if not authenticated
+    Write-Host "Creating Rule 5 (Header Overwrite)..." -ForegroundColor Cyan
+    az afd rule create `
+        --resource-group $Config.RESOURCE_GROUP `
+        --rule-set-name $Config.RULESET_NAME `
+        --profile-name $Config.FRONT_DOOR_PROFILE `
+        --order 6 `
+        --match-variable RequestHeader `
+        --selector "x-enforcer-auth" `
+        --operator Equal `
+        --negate-condition true `
+        --match-values $Config.SECRET_KEY `
+        --transforms Trim `
+        --rule-name HSUnenforcedRequest `
+        --action-name ModifyRequestHeader `
+        --header-action Overwrite `
+        --header-name "x-functions-key" `
+        --header-value "<enter unique value before deployment>" `
+        --match-processing-behavior Stop | Out-Null
+
+    az afd rule action add `
+        --resource-group $Config.RESOURCE_GROUP `
+        --rule-set-name $Config.RULESET_NAME `
+        --profile-name $Config.FRONT_DOOR_PROFILE `
+        --rule-name HSUnenforcedRequest `
+        --action-name RouteConfigurationOverride `
+        --origin-group HSEnforcer `
+        --forwarding-protocol MatchRequest `
+        --enable-caching false | Out-Null
+
+    # Create the route
+    Write-Host "Creating Front Door Route..." -ForegroundColor Cyan
+    az afd route create `
+        -g $Config.RESOURCE_GROUP `
+        --endpoint-name $Config.ENDPOINT `
+        --profile-name $Config.FRONT_DOOR_PROFILE `
+        --route-name HumanSecurityRoute `
+        --rule-sets $Config.RULESET_NAME `
+        --origin-group BackendOrigin `
+        --supported-protocols Http Https `
+        --link-to-default-domain Enabled `
+        --forwarding-protocol MatchRequest `
+        --https-redirect Disabled | Out-Null
+
+    Write-Host "✅ Route and Ruleset configuration complete." -ForegroundColor Green
+
 }
